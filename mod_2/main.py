15f2f94e37e2c54cebe3d84dc04789f5671c99a3
Dominik <dominikzazula@interia.pl>
2023-01-20 19:14:24 +0100

    mod 2 homework 2 task 2 and 3

--- mod_2/main.py
+++ mod_2/main.py
@@ -1,12 +1,19 @@
+import random
+
+
 class Product:
-    def __int__(self, name, category, price):
+
+    def __init__(self, name: str, category: str, unit_price: float):
         self.name = name
         self.category = category
-        self.price = price
+        self.unit_price = unit_price
+
+    def print_product(self):
+        print("nazwa produktu: ", self.name, " kategoria: ", self.category, "cena: ", self.unit_price)
 
 
 class Order:
-    def __int__(self, name, surname, products=None):
+    def __init__(self, name: str, surname: str, products=None):
         self.name = name
         self.surname = surname
         if products is None:
@@ -16,36 +23,66 @@ class Order:
         total_price = 0
         for product in products:
             total_price += product.unit_price
-        self.total_price = total_price
+        self.total_price = round(total_price, 2)
+
+    def print_order(self):
+        print("=" * 20)
+        print("Zamawia pan/pani ", self.name, " ", self.surname)
+        for product in self.products:
+            product.print_product()
+        print("cena calego zamowienia wynosi: ", self.total_price)
+        print("=" * 20)
 
 
 class Apple:
-    def __int__(self, species_name, size, price):
+    def __init__(self, species_name: str, size: str, price: float):
         self.species_name = species_name
         self.size = size
         self.price = price
 
 
 class Potato:
-    def __int__(self, species_name, size, price):
+    def __init__(self, species_name: str, size: str, price: float):
         self.species_name = species_name
         self.size = size
         self.price = price
 
 
-if __name__ == "__main__":
-    kosiarka = Product("kosiarka", "maszyna", 1299.99)
+def generate_order():
+    number_of_products = random.randint(1, 10)
+    products = []
+    for product_number in range(number_of_products):
+        name = f"Product-{product_number}"
+        category = f"Categoria-{product_number}"
+        products.append(Product(name, category, round(random.uniform(1, 200), 2)))
+    order = Order("arek", "kowalski", products)
+    return order
+
+
+def run():
+    order = generate_order()
+    order.print_order()
+    kosiarka = Product(name="kosiarka", category="maszyna", unit_price=1299.99)
     telefon = Product("samsung", "telefon", 699.99)
     washing_machine = Product("pralka", "rtv", 3999.99)
     produkty = [kosiarka, telefon, washing_machine]
+    kosiarka.print_product()
+    telefon.print_product()
+    washing_machine.print_product()
 
     order = Order("Dominik", "Zazula", produkty)
     order1 = Order("jan", "Kowalski")
+    order.print_order()
+    order1.print_order()
 
-    green_apple = Apple("apple", 34, 3.99)
-    old_apple = Apple("old apple", 32, 1.99)
-    red_apple = Apple("red apple", 31, 4.99)
+    green_apple = Apple("apple", "M", 3.99)
+    old_apple = Apple("old apple", "S", 1.99)
+    red_apple = Apple("red apple", "M", 4.99)
 
-    green_potato = Potato("bad potato", 24, 3.99)
-    old_potato = Potato("old potato", 32, 1.99)
-    sweat_potato = Apple("sweat potato", 31, 4.99)
+    green_potato = Potato("bad potato", "S", 3.99)
+    old_potato = Potato("old potato", "S", 1.99)
+    sweat_potato = Apple("sweat potato", "S", 4.99)
+
+
+if __name__ == "__main__":
+    run()
