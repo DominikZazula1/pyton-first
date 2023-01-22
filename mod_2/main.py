38998fae16d8e3e05e961fd73ad4f942c7ed6c8b
Dominik <dominikzazula@interia.pl>
2023-01-22 13:12:12 +0100

    mod 2 homework 5 finished

--- mod_2/main.py
+++ mod_2/main.py
@@ -1,3 +1,12 @@
+# Zabezpiecz listę pozycji w zamówieniu i łączną wartość zamówienia przed utratą spójności.
+#
+# W tym celu:
+#
+# Zamień listę pozycji w zamówieniu na zmienną prywatną. Zamień również metodę obliczającą łączny koszt zamówienia na
+# prywatną. Dodaj metodę publiczną umożliwiającą dodanie nowego produktu do zamówienia (potrzebne będą informacje o
+# produkcie i ilości). Pamiętaj wywołać ponownie przeliczenie łącznej wartości zamówienia.
+
+from shop.product import Product
 from shop.order import generate_order
 
 
@@ -5,10 +14,9 @@ def run():
     order = generate_order()
     print(order)
     print(len(order))
-    order1 = order
-    print(order == order1)
-    order2 = generate_order()
-    print(order == order2)
+    order.add_order_element(Product("allla", "ddddddad", 12.13), 32)
+    print(order)
+    print(len(order))
 
 
 if __name__ == "__main__":
--- mod_2/shop/order.py
+++ mod_2/shop/order.py
@@ -10,39 +10,45 @@ class Order:
         self.surname = surname
         if order_element is None:
             order_element = []
-        self.order_element = order_element
+        self._order_element = order_element
 
-        total_price = 0
-        for product in order_element:
-            total_price += product.total_price()
-        self.total_price = round(total_price, 2)
+        self.total_price = self._calculate_total_price()
 
     def __str__(self):
         return_valiu = "=" * 20
         return_valiu += f"\nZamawia pan/pania  {self.name} {self.surname}"
-        for product in self.order_element:
+        for product in self._order_element:
             return_valiu += f"\n  {str(product)}"
         return_valiu += f"\ncena calego zamowienia wynosi: {self.total_price} \n  "
         return_valiu += "=" * 20
         return return_valiu
 
     def __len__(self):
-        return len(self.order_element)
+        return len(self._order_element)
 
     def __eq__(self, other):
         if self.__class__ != other.__class__:
             return NotImplemented
 
-        if len(self.order_element) != len(other.order_element):
+        if len(self._order_element) != len(other.order_element):
             return False
 
         if self.name != other.name or self.surname != other.surname:
             return False
 
-        for order_element in self.order_element:
+        for order_element in self._order_element:
             if order_element not in other.order_element:
                 return False
         return True
+
+    def _calculate_total_price(self):
+        total_price = 0
+        for product in self._order_element:
+            total_price += product.total_price()
+        return round(total_price, 2)
+
+    def add_order_element(self, product: Product, quantity: int):
+        self._order_element.append(OrderElement(product, quantity))
 
 
 def generate_order():
@@ -51,6 +57,7 @@ def generate_order():
     for product_number in range(number_of_order_element):
         name = f"Product-{product_number}"
         category = f"Categoria-{product_number}"
-        order_element.append(OrderElement(Product(name, category, round(random.uniform(1, 200), 2)), random.randint(1, 20)))
+        order_element.append(
+            OrderElement(Product(name, category, round(random.uniform(1, 200), 2)), random.randint(1, 20)))
     order = Order("arek", "kowalski", order_element)
     return order
