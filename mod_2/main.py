f9c52e73fe8527f91fd439bcb0e37589c4090b76
Dominik <dominikzazula@interia.pl>
2023-01-27 20:39:35 +0100

    mod 3 homework 3 finished

--- mod_2/main.py
+++ mod_2/main.py
@@ -1,26 +1,12 @@
 
 from shop.data_generator import generate_order_elements
-from shop.order import Order
+from shop.product import ExpiringProduct
 
 
 def run():
-    order_elements = generate_order_elements()
-    order = Order("Dominik", "ZZIZ", order_elements)
-    print("imeie : ", order.name)
-    print("nazwiska : ", order.surname)
-    print("cena : ", order.total_price, "zl")
-    print("Lista: \n")
-    for element in order.order_elements:
-        print(element)
-    order1 = Order("Maria", "Bem")
-    order_elements1 = generate_order_elements(number_of_products=20)
-    order1.order_elements = order_elements1
-    print("imeie : ", order1.name)
-    print("nazwiska : ", order1.surname)
-    print("cena : ", order1.total_price, "zl")
-    print("Lista: \n")
-    for element in order1.order_elements:
-        print(element)
+    product = ExpiringProduct("Mleko", "picie", 12.13, 2022, 1)
+    print(product.dose_expire(2023))
+    print(product.dose_expire(2024))
 
 
 if __name__ == "__main__":
--- mod_2/shop/product.py
+++ mod_2/shop/product.py
@@ -17,3 +17,13 @@ class Product:
                     self.category_name == other.category_name and
                     self.unit_price == other.unit_price)
 
+
+class ExpiringProduct(Product):
+
+    def __init__(self, name: str, category: str, unit_price: float, year_of_production, years_of_validity):
+        super().__init__(name, category, unit_price)
+        self.year_of_production = year_of_production
+        self.years_of_validity = years_of_validity
+
+    def dose_expire(self, year):
+        return year > self.year_of_production + self.years_of_validity
