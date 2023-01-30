308b2d44c9643a126a0d7eac272a8db80dd84856
Dominik <dominikzazula@interia.pl>
2023-01-30 22:07:23 +0100

    mod 4 homework 3 finished

--- mod_2/main.py
+++ mod_2/main.py
@@ -1,37 +1,22 @@
-
 import random
 
 
-def hellow_to_you():
-    name = input("podaj swoje imie: ").strip()
-    surname = input("podaj swoje nazwisko: ").strip()
-    # print(f"Nazywasz się {name} {surname} - jak miło Cię poznać :)")
-    print("Nazywasz się {} {} - jak miło Cię poznać :)".format(name, surname))
-
-
-def id_generator():
-    return str(random.randint(1, 9999)).zfill(5)
-
-
-def your_favorite_colors(*args):
-    colors = ", ".join(args)
-    if "niebieski" in colors.lower():
-        print("lubisz niebieski!")
-    else:
-        print("nie lubisz niebieskiego")
-
-
 def run():
-    # Napisz funkcję, która wczyta od użytkownika jej/jego imię i nazwisko,
-    # “wyczyści je” z białych znaków na początku i końcu tekstu, a następnie wypisze jakieś zdanie z tymi danymi np.
-    # “Nazywasz się {first_name} {last_name} - jak miło Cię poznać :)”
-    hellow_to_you()
-    # Napisz funkcję generującą losowy identyfikator. Identyfikator powinien być w formacie 00001,
-    # tzn. zawsze o długości pięciu znaków, dopełniony z lewej strony odpowiednią liczbą zer.
-    print(id_generator())
-    # Wczytaj od użytkownika jej/jego ulubione kolory (jako jeden napis, np. rozdzielony przecinkiem).
-    # Sprawdź, czy znajduje się wśród nich niebieski, a następnie wypisz odpowiedni komunikat.
-    print(your_favorite_colors("brozowy", "czarny", "czerwony"))
+    # Zadanie nr 1 Używając list comprehensions wygeneruj listy zawierające liczby od 1 do 30 podzielne kolejno przez
+    # 3 oraz przez 5. To znaczy, że na pierwszej liście powinny znaleźć się liczby od 1 do 30 podzielne przez 3,
+    # a na drugiej liście liczby od 1 do 30 podzielne przez 5. Następnie, połącz obie listy w jedną i wypisz wynik.
+    # Zadanie nr 2 Wczytaj od użytkownika listę ulubionych sportów, a następnie stosując slicing wypisz co drugi,
+    # pomijając pierwszy sport z listy.
+    numbers_3 = [number for number in range(31) if number % 3 == 0]
+    numbers_5 = [number for number in range(31) if number % 5 == 0]
+    numbers_3 += numbers_5
+    favorite_sports = []
+    sport = "a"
+    while sport != "":
+        sport = input("podaj ulubiony sport:")
+        favorite_sports.append(sport)
+    print(numbers_3)
+    print(favorite_sports[1::2])
 
 
 if __name__ == "__main__":
