476e4c61ccf9c480e832b59e06ae5a94d4172237
Dominik <dominikzazula@interia.pl>
2023-02-02 00:42:05 +0100

    mod 4 homework 9 finished

--- mod_2/main.py
+++ mod_2/main.py
@@ -1,36 +1,20 @@
-# Napisz test sprawdzający poprawność wykonanej w poprzednim module metody magicznej __eq__ w klasie Product - czyli 
-# porównywania produktów.
-#
-# Dla przypomnienia, dwa produkty są sobie równe, gdy mają taką samą nazwę, taką samą kategorię i taką samą cenę 
-# jednostkową.
-#
-# Wykorzystaj tuple do przygotowania różnych zestawów parametrów danych do algorytmu testującego.
-from mod_2.shop.product import Product
+from collections import namedtuple
 
 
-def run_test_eq():
-    parameters = [
-        ("A", "B", 10, "A", "B", 10, True),
-        ("A", "B", 10, "C", "B", 10, False),
-        ("A", "B", 10, "A", "C", 10, False),
-        ("A", "B", 10, "A", "B", 11, False),
-        ("A", "B", 10, "C", "D", 10, False),
-        ("A", "B", 10, "C", "B", 11, False),
-        ("A", "B", 10, "A", "C", 11, False),
-        ("A", "B", 10, "D", "C", 11, False),
+def run():
+    Apple = namedtuple("Apple", ["species_name", "size", "price"])
 
-    ]
+    apple = Apple("green apple", "M", 11.1)
+    print(apple.species_name)
+    print(apple.size)
+    print(apple.price)
+    for x in range(3):
+        print(apple[x])
+    for item in apple:
+        print(item)
 
-    for params in parameters:
-        name, category, price, other_name, other_category, other_price, equality = params
-        result = Product(name, category, price) == Product(other_name, other_category, other_price)
 
-        if result == equality:
-            print("OK")
-        else:
-            print(
-                f"Błąd! Dla parametrów: {params} wynik promocji to {result} zamiast {equality}")
 
 
 if __name__ == "__main__":
-    run_test_eq()
+    run()
