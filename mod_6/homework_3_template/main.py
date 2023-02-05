c2c086cbe16060299fa4419bfff84fde427ca528
Dominik <dominikzazula@interia.pl>
2023-02-05 20:41:15 +0100

    mod 6 homework 4 finished

--- mod_6/homework_3_template/main.py
+++ mod_6/homework_3_template/main.py
@@ -5,11 +5,23 @@
 #  - W celu kontroli dostępności towarów klasa Store, posiada zdefiniowaną listę dostępnych produktów i ich liczbę.
 #  Podczas dodawania kolejnych produktów do zamówienia aktualizuj ich stan w Store i gdyby okazało się,
 #  że nie można zrealizować oczekiwań klienta wyrzuć odpowiedni wyjątek i obsłuż go komunikatem dla użytkownika.
-from shop import user_interface
+from shop.errors import NotValidInput
+
+
+def three_letters_of_name(string):
+    if len(string) != 3:
+        raise NotValidInput("litery maja byc 3")
 
 
 def run_homework():
-    user_interface.handle_customer()
+    try:
+        three_letters_of_name(input("jak sie zaczyna twoje imie? podaj 3 litery: "))
+    except NotValidInput as error:
+        print(error.args)
+    else:
+        print("podane dane sa prawidlowe")
+    finally:
+        print("koniec programu")
 
 
 if __name__ == '__main__':
