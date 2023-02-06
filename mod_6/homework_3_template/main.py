520bfba96bf01a41aae997822d981cc8b013b7e1
Dominik <dominikzazula@interia.pl>
2023-02-06 23:24:02 +0100

    mod 6 homework 7 task 1 and 2

--- mod_6/homework_3_template/main.py
+++ mod_6/homework_3_template/main.py
@@ -1,9 +1,12 @@
-
+from shop.persistence import load_store, save_store
+from shop.store import Store
 from shop import user_interface
 
 
 def run_homework():
+    Store.AVAILABLE_PRODUCTS = load_store()
     user_interface.handle_customer()
+    save_store()
 
 
 if __name__ == '__main__':
--- mod_6/homework_3_template/shop/persistence.py
+++ mod_6/homework_3_template/shop/persistence.py
@@ -1,4 +1,39 @@
+import csv
 import os
+
+from .product import ProductCategory
+from .store import AvailableProduct, Store
+
+
+def load_store(file_name="store.csv"):
+    with open(file_name, newline="") as store_file:
+        csv_reader = csv.reader(store_file)
+        next(csv_reader)
+        return [
+            AvailableProduct(
+                name=row[0],
+                category=ProductCategory[row[1]],
+                unit_price=float(row[2]),
+                identifier=int(row[3]),
+                quantity=int(row[4])
+            )
+            for row in csv_reader
+        ]
+
+
+def save_store(file_name="store.csv"):
+    with open(file_name, mode="w", newline="") as store_file:
+        csv_writer = csv.writer(store_file)
+        # csv_writer = csv.writer(store_file, delimiter=";", quotechar="|", quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(["name", "category", "unit_price", "identifier", "quantity"])
+        for available_product in Store.AVAILABLE_PRODUCTS:
+            csv_writer.writerow([
+                available_product.product.name,
+                available_product.product.category,
+                available_product.product.unit_price,
+                available_product.product.identifier,
+                available_product.quantity
+            ])
 
 
 def save_to_file(order):
